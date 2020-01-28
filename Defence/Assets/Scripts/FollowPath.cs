6fdc32fb7fc35e9b6c01765e36b56fa3ddc8c472
Froly123 <sleepryan@gmail.com>
2020-01-28 11:54:51 -0400

    Added multiple pathways for enemys added Start menu, Level creation begin tomorrow

--- Defence/Assets/Scripts/FollowPath.cs
+++ Defence/Assets/Scripts/FollowPath.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 public class FollowPath : MonoBehaviour
 {
     [SerializeField]
-    private Transform[] routes;
+    private Transform[] route1;
+    [SerializeField]
+    private Transform[] route2;
+    
+    public int Numofpath;
 
     private int routeToGo;
     private float tParam;
@@ -18,6 +22,8 @@ public class FollowPath : MonoBehaviour
 void Awake()
     {
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+        Numofpath = Random.Range(1,Numofpath + 1);
+
     }
     // Start is called before the first frame update
     void Start()
@@ -33,23 +39,32 @@ void Awake()
     {
         if (!Original)
         {
-            if (coroutineAllowed)
+            if (coroutineAllowed && Numofpath ==1)
             {
-                StartCoroutine(GoByTheRoute(routeToGo));
+                StartCoroutine(GoByTheRoute1(routeToGo));
+            }
+            if (coroutineAllowed && Numofpath == 2)
+            {
+                StartCoroutine(GoByTheRoute2(routeToGo));
             }
 
 
         }
     }
 
-    private IEnumerator GoByTheRoute(int routeNumber)
+    private IEnumerator GoByTheRoute1(int routeNumber)
     {
         coroutineAllowed = false;
+
+
         Vector2 theScale = transform.localScale;
-        Vector2 p0 = routes[routeNumber].GetChild(0).position;
-        Vector2 p1 = routes[routeNumber].GetChild(1).position;
-        Vector2 p2 = routes[routeNumber].GetChild(2).position;
-        Vector2 p3 = routes[routeNumber].GetChild(3).position;
+        Vector2 p0 = route1[routeNumber].GetChild(0).position;
+        Vector2 p1 = route1[routeNumber].GetChild(1).position;
+        Vector2 p2 = route1[routeNumber].GetChild(2).position;
+        Vector2 p3 = route1[routeNumber].GetChild(3).position;
+        
+        
+        
         if(p0.x > p3.x)
         {
             mySpriteRenderer.flipX = true;
@@ -73,7 +88,49 @@ void Awake()
 
         routeToGo += 1;
 
-        if (routeToGo > routes.Length - 1)
+        if (routeToGo > route1.Length - 1)
+            routeToGo = 0;
+
+        coroutineAllowed = true;
+    }
+
+    private IEnumerator GoByTheRoute2(int routeNumber)
+    {
+        coroutineAllowed = false;
+
+
+        Vector2 theScale = transform.localScale;
+        Vector2 p0 = route2[routeNumber].GetChild(0).position;
+        Vector2 p1 = route2[routeNumber].GetChild(1).position;
+        Vector2 p2 = route2[routeNumber].GetChild(2).position;
+        Vector2 p3 = route2[routeNumber].GetChild(3).position;
+
+
+
+        if (p0.x > p3.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            //Movig Right
+
+        }
+
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speedModifier;
+
+            EnemyPos = Mathf.Pow(1 - tParam, 3) * p0 + 3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
+
+            transform.position = EnemyPos;
+            yield return new WaitForEndOfFrame();
+        }
+        tParam = 0f;
+
+        routeToGo += 1;
+
+        if (routeToGo > route2.Length - 1)
             routeToGo = 0;
 
         coroutineAllowed = true;
