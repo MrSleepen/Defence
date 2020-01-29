ddddc9e8e804f10ad8bdf0080d91f9ad3018015c
Froly123 <sleepryan@gmail.com>
2020-01-29 11:14:37 -0400

    Adjusted spawner for multiple enemys and random pathing, created level 1

--- Defence/Assets/Scripts/SpawnEnemy.cs
+++ Defence/Assets/Scripts/SpawnEnemy.cs
@@ -13,6 +13,8 @@ public class SpawnEnemy : MonoBehaviour
     public float spawnDelay;
     public int MaxSpawn;
     public int CurrentSpawned;
+    public int openCreatures;
+    public int RandomNumber;
     
     
     // Start is called before the first frame update
@@ -20,11 +22,17 @@ public class SpawnEnemy : MonoBehaviour
     {
         InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
     }
+    void FixedUpdate()
+    {
+        RandomNumber = Random.Range(1, openCreatures+1);
+    }
 
     // Update is called once per frame
    public void SpawnObject()
     {
-        if (CurrentSpawned < MaxSpawn  )
+        
+
+        if (CurrentSpawned < MaxSpawn && openCreatures >= 1 && RandomNumber == 1)
         {
             spawned = Instantiate(spawnee[1], transform.position, transform.rotation);
             ++CurrentSpawned;
@@ -33,9 +41,82 @@ public class SpawnEnemy : MonoBehaviour
 
             spawned.GetComponent<FollowPath>().NotOriginal();
 
+            
 
         }
-        else
+
+         if (CurrentSpawned < MaxSpawn && openCreatures >= 2 && RandomNumber == 2)
+        {
+            spawned = Instantiate(spawnee[2], transform.position, transform.rotation);
+            ++CurrentSpawned;
+            PlayerPrefs.SetInt("RoundSpawnCount", PlayerPrefs.GetInt("RoundSpawnCount") + 1);
+            PlayerPrefs.SetInt("Spawned1", 1);
+
+            spawned.GetComponent<FollowPath>().NotOriginal();
+            
+        }
+
+        if (CurrentSpawned < MaxSpawn && openCreatures >= 3 && RandomNumber == 3)
+        {
+            spawned = Instantiate(spawnee[3], transform.position, transform.rotation);
+            ++CurrentSpawned;
+            PlayerPrefs.SetInt("RoundSpawnCount", PlayerPrefs.GetInt("RoundSpawnCount") + 1);
+            PlayerPrefs.SetInt("Spawned1", 1);
+
+            spawned.GetComponent<FollowPath>().NotOriginal();
+
+        }
+        if (CurrentSpawned < MaxSpawn && openCreatures >= 4 && RandomNumber == 4)
+        {
+            spawned = Instantiate(spawnee[4], transform.position, transform.rotation);
+            ++CurrentSpawned;
+            PlayerPrefs.SetInt("RoundSpawnCount", PlayerPrefs.GetInt("RoundSpawnCount") + 1);
+            PlayerPrefs.SetInt("Spawned1", 1);
+
+            spawned.GetComponent<FollowPath>().NotOriginal();
+
+        }
+        if (CurrentSpawned < MaxSpawn && openCreatures >= 5 && RandomNumber == 5)
+        {
+            spawned = Instantiate(spawnee[5], transform.position, transform.rotation);
+            ++CurrentSpawned;
+            PlayerPrefs.SetInt("RoundSpawnCount", PlayerPrefs.GetInt("RoundSpawnCount") + 1);
+            PlayerPrefs.SetInt("Spawned1", 1);
+
+            spawned.GetComponent<FollowPath>().NotOriginal();
+
+        }
+        if (CurrentSpawned < MaxSpawn && openCreatures >= 6 && RandomNumber == 6)
+        {
+            spawned = Instantiate(spawnee[6], transform.position, transform.rotation);
+            ++CurrentSpawned;
+            PlayerPrefs.SetInt("RoundSpawnCount", PlayerPrefs.GetInt("RoundSpawnCount") + 1);
+            PlayerPrefs.SetInt("Spawned1", 1);
+
+            spawned.GetComponent<FollowPath>().NotOriginal();
+
+        }
+        if (CurrentSpawned < MaxSpawn && openCreatures >= 7 && RandomNumber == 7)
+        {
+            spawned = Instantiate(spawnee[7], transform.position, transform.rotation);
+            ++CurrentSpawned;
+            PlayerPrefs.SetInt("RoundSpawnCount", PlayerPrefs.GetInt("RoundSpawnCount") + 1);
+            PlayerPrefs.SetInt("Spawned1", 1);
+
+            spawned.GetComponent<FollowPath>().NotOriginal();
+
+        }
+        if (CurrentSpawned < MaxSpawn && openCreatures >= 8 && RandomNumber == 8)
+        {
+            spawned = Instantiate(spawnee[8], transform.position, transform.rotation);
+            ++CurrentSpawned;
+            PlayerPrefs.SetInt("RoundSpawnCount", PlayerPrefs.GetInt("RoundSpawnCount") + 1);
+            PlayerPrefs.SetInt("Spawned1", 1);
+
+            spawned.GetComponent<FollowPath>().NotOriginal();
+
+        }
+        if ( CurrentSpawned >= MaxSpawn)
         {
             StopSpawning = true;
         }
